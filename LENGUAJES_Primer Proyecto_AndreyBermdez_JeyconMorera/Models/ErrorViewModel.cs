f3677d073e33f35fc1db13c4041cffae91ef207a
ABA11 <abermudeza@ucatolica.ac.cr>
2019-03-14 12:38:00 -0600

    Agregar archivos de proyecto.

---
+++ LENGUAJES_Primer Proyecto_AndreyBermdez_JeyconMorera/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace LENGUAJES_Primer_Proyecto_AndreyBermdez_JeyconMorera.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
